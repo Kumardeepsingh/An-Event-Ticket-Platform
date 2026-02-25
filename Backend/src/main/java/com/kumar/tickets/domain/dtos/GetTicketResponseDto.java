cecb4651fdabe067417fe98f3bb004faefbcd1d7
Kumardeep Singh <Kumardeepsingh@student.kpu.ca>
2026-02-24 17:44:44 -0800

    Implemented the get ticket service layer, Dto and mapper

---
+++ Backend/src/main/java/com/kumar/tickets/domain/dtos/GetTicketResponseDto.java
@@ -0,0 +1,23 @@
+package com.kumar.tickets.domain.dtos;
+
+import com.kumar.tickets.domain.enities.TicketStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetTicketResponseDto {
+    private UUID id;
+    private TicketStatusEnum status;
+    private double price;
+    private String description;
+    private String eventName;
+    private String eventVenue;
+    private LocalDateTime eventStart;
+    private LocalDateTime EventEnd;
+}
--- Backend/src/main/java/com/kumar/tickets/mappers/TicketMapper.java
+++ Backend/src/main/java/com/kumar/tickets/mappers/TicketMapper.java
@@ -1,10 +1,12 @@
 package com.kumar.tickets.mappers;
 
+import com.kumar.tickets.domain.dtos.GetTicketResponseDto;
 import com.kumar.tickets.domain.dtos.ListTicketResponseDto;
 import com.kumar.tickets.domain.dtos.ListTicketTicketTypeResponseDto;
 import com.kumar.tickets.domain.enities.Ticket;
 import com.kumar.tickets.domain.enities.TicketType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,5 +14,13 @@ public interface TicketMapper {
     ListTicketTicketTypeResponseDto toListTicketTicketTypeResponseDto(TicketType ticketType);
 
     ListTicketResponseDto toListTicketResponseDto(Ticket ticket);
+
+    @Mapping(target = "price", source = "ticket.ticketType.price")
+    @Mapping(target = "description", source = "ticket.ticketType.description")
+    @Mapping(target = "eventName", source = "ticket.ticketType.event.name")
+    @Mapping(target = "eventVenue", source = "ticket.ticketType.event.venue")
+    @Mapping(target = "eventStart", source = "ticket.ticketType.event.start")
+    @Mapping(target = "eventEnd", source = "ticket.ticketType.event.end")
+    GetTicketResponseDto toGetTicketResponseDto(Ticket ticket);
 
 }
